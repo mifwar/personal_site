5a14eaae285ab18fcb50e38a3a08a0e076ae5431
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 00:27:21 +0700

    center the div

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <>
+    <div className="flex justify-center text-center flex-col">
       <h4 className="text-xs text-black dark:text-white font-Poppins mt-7 py-1">
         Hosted in{" "}
         <a
@@ -45,7 +45,7 @@ const Footer = () => {
       <h4 className="text-xs text-black dark:text-white font-Poppins py-1">
         MIT License © 2022 Miftahul Anwar
       </h4>
-    </>
+    </div>
   );
 };
 

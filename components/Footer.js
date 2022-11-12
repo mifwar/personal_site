c37d3bede9a7090c47c6eba5aff7f0e2da359bd0
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 01:11:41 +0700

    add missing space

--- components/Footer.js
+++ components/Footer.js
@@ -17,9 +17,12 @@ const Footer = () => {
   return (
     <div className="flex justify-center text-center flex-col">
       <h4 className="text-xs text-black dark:text-white font-Poppins mt-7 py-1">
-        Hosted in {footerLink("https://vercel.com/", "Vercel")}
-        and built using {footerLink("https://nextjs.org/", "Next.js")}
-        and {footerLink("https://tailwindcss.com/", "Tailwind CSS")}
+        Hosted in
+        {footerLink("https://vercel.com/", " Vercel ")}
+        and built using
+        {footerLink("https://nextjs.org/", " Next.js ")}
+        and
+        {footerLink("https://tailwindcss.com/", " Tailwind CSS")}
       </h4>
       <h4 className="text-xs text-black dark:text-white font-Poppins py-1">
         MIT License © 2022 Miftahul Anwar

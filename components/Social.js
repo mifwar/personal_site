cb508bd7f2ac4713331e1cfe61053e75eb929fc0
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 00:28:24 +0700

    add hover in social icons

--- components/Social.js
+++ components/Social.js
@@ -9,9 +9,9 @@ import {
 
 const Social = () => {
   return (
-    <div className="text-lg flex justify-center mx-auto text-black dark:text-white mt-20">
+    <div className="text-lg flex justify-center mx-auto mt-20">
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="mailto:hello@mifwar.com"
         target="_blank"
         rel="noopener noreferrer"
@@ -19,7 +19,7 @@ const Social = () => {
         <FaEnvelope />
       </a>
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="https://github.com/mifwar"
         target="_blank"
         rel="noopener noreferrer"
@@ -27,7 +27,7 @@ const Social = () => {
         <FaGithub />
       </a>
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="https://hackerrank.com/2210171043_mif"
         target="_blank"
         rel="noopener noreferrer"
@@ -35,7 +35,7 @@ const Social = () => {
         <FaHackerrank />
       </a>
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="https://www.linkedin.com/in/miftahulanwar/"
         target="_blank"
         rel="noopener noreferrer"
@@ -43,7 +43,7 @@ const Social = () => {
         <FaLinkedin />
       </a>
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="https://twitter.com/mif_war"
         target="_blank"
         rel="noopener noreferrer"
@@ -51,7 +51,7 @@ const Social = () => {
         <FaTwitter />
       </a>
       <a
-        className="px-3"
+        className="px-3 text-black dark:text-white hover:text-blue-500 dark:hover:text-blue-400"
         href="https://instagram.com/mif.war"
         target="_blank"
         rel="noopener noreferrer"

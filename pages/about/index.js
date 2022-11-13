3708ba378d7cb94b2cdc014d142e843503a94df5
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 15:59:22 +0700

    add some paragraphs in about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import profile from "../../public/MiftahulAnwarCropped.jpg";
+import about from "../../public/MiftahulAnwarAbout.jpeg";
 import React, { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { useThemeContext } from "../../utils/darkMode";
 import Navbar from "../../components/Navbar";
+import Social from "../../components/Social";
+import Footer from "../../components/Footer";
+import { customLink } from "../../utils/utils";
 
 const About = () => {
   const [darkMode, setDarkMode] = useThemeContext();
@@ -37,9 +39,7 @@ const About = () => {
   return (
     <div className={darkMode ? "dark" : ""}>
       <Head>
-        <title>Miftahul Anwar</title>
-        <meta name="description" content="Miftahul Anwar's personal site" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>About</title>
       </Head>
       <main className="bg-white dark:bg-gray-900">
         <section className="min-h-screen">
@@ -49,9 +49,65 @@ const About = () => {
             changeDarkMode={setDarkMode}
             path="about"
           />
-          <div className="flex justify-center items-center h-screen -my-16 dark:text-white">
-            About Page
+          <div className="flex justify-center items-start xs:items-center dark:text-white my-10 xs:flex-col-reverse">
+            <div className="mx-10 max-w-lg text-justify font-Poppins text-sm">
+              <p className="my-3">
+                Hello! My name is Miftahul Anwar and you can call me Miftah.
+              </p>
+              <p className="my-3">
+                My programming journey is started when i became a Computer
+                Engineering student at
+                {customLink(
+                  "https://www.pens.ac.id/",
+                  " PENS (Politeknik Elektronika Negeri Surabaya)"
+                )}
+                .
+              </p>
+              <p className="my-3">
+                During my college life, i spent my first 3 years in
+                {customLink("https://erosrobotic.id/", " EROS ")} as a
+                programmer. EROS is a Humanoid Robot Soccer Team under the
+                management of PENS Robotics Team. EROS is developed to compete
+                in Humanoid Robot Soccer League in
+                {customLink(
+                  "https://kontesrobotindonesia.id",
+                  " Kontes Robot Indonesia "
+                )}
+                and
+                {customLink(
+                  "https://humanoid.robocup.org/",
+                  " Robocup Humanoid League"
+                )}
+                .
+              </p>
+              <p className="my-3">
+                While at EROS, i created and involved in some projects. My first
+                project is creating an app that able to wirelessly track the
+                robot states while robot in the field. I also involved to
+                develop a decision making algorithm, an object detection system
+                using computer vision, and a teamwork algorithm.
+              </p>
+              <p className="my-3">
+                In my last year at college, i became a freelance software
+                engineer and handled some projects that mostly about Desktop
+                App, Computer Vision, and IoT.
+              </p>
+              <p className="my-3">
+                Fast forward to today, I am currently working for
+                {customLink("https://www.zenius.net/", " Zenius Education ")}
+                as a Clojure Developer. I created a Clojure app to speed up the
+                tutor in Zenius to create a school exercise problems.
+              </p>
+            </div>
+            <Image
+              className="rounded-lg m-5  w-56 xs:w-48"
+              src={about}
+              alt="Miftahul Anwar"
+              placeholder="blur"
+            />
           </div>
+          <Social />
+          <Footer />
         </section>
       </main>
     </div>

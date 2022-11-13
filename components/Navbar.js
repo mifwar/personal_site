2a7caec1fb14d21eddd93543202904a067f01828
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2022-11-13 23:10:30 +0700

    modify to fit react-scrollbar style

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,48 @@
+import React, { useState } from "react";
+
 import { BsFillMoonFill, BsSunFill } from "react-icons/bs";
-import Link from "next/link";
+import { Link, animateScroll as scroll } from "react-scroll";
+
+// import Link from "next/link";
 
 import Sidebar from "./Sidebar";
 
 const Navbar = (props) => {
+  const [path, setPath] = useState("b_home");
+
   const navStyle =
-    "z-10 flex items-center max-w-4xl mx-auto -top-1 sticky bg-white/90 dark:bg-gray-900/90";
-  const navStyleBlur =
-    "z-10 flex items-center max-w-4xl mx-auto -top-1 sticky backdrop-blur-sm bg-white/90 dark:bg-gray-900/90";
-  const { isMobile, darkMode, changeDarkMode, path } = props;
+    "z-10 flex items-center max-w-4xl mx-auto -top-1 sticky backdrop-blur-sm bg-white/80 dark:bg-gray-900/80";
+  const { isMobile, darkMode, changeDarkMode } = props;
   const links = [
-    { id: 1, href: "/", path: "home", txt: "Home" },
-    { id: 2, href: "/about", path: "about", txt: "About" },
-    { id: 3, href: "/projects", path: "projects", txt: "Projects" },
-    { id: 4, href: "/achievements", path: "achievements", txt: "Achievements" },
+    { id: 1, href: "home", path: "b_home", tx: "Home" },
+    { id: 2, href: "about", path: "b_about", tx: "About" },
+    { id: 3, href: "projects", path: "b_projects", tx: "Projects" },
+    { id: 4, href: "achievements", path: "b_achievements", tx: "Achievements" },
   ];
   return (
-    <nav className={isMobile ? navStyle : navStyleBlur}>
+    <nav id="nav" className={navStyle}>
       {isMobile ? (
         <Sidebar isDark={darkMode} path={path} />
       ) : (
         <div className="xs:hidden ml-auto pl-10">
           {links.map((link) => (
             <Link
+              id={link.path}
+              activeClass="active"
               key={link.id}
-              href={link.href}
+              to={link.href}
+              offset={-90}
+              onClick={() => {
+                setPath(link.path);
+                for (let i = 0; i < links.length; i++) {
+                  document.getElementById(links[i].path).className = darkMode
+                    ? "nav-menu-big dark"
+                    : "nav-menu-big";
+                }
+                document.getElementById(link.path).className = darkMode
+                  ? "nav-menu-big dark-active"
+                  : "nav-menu-big active";
+              }}
               className={
                 path === link.path
                   ? darkMode
@@ -35,7 +53,7 @@ const Navbar = (props) => {
                   : "nav-menu-big"
               }
             >
-              {link.txt}
+              {link.tx}
             </Link>
           ))}
         </div>
@@ -44,10 +62,18 @@ const Navbar = (props) => {
         {darkMode ? (
           <BsSunFill
             className="text-white"
-            onClick={() => changeDarkMode(!darkMode)}
+            onClick={() => {
+              setPath(path);
+              changeDarkMode(!darkMode);
+            }}
           />
         ) : (
-          <BsFillMoonFill onClick={() => changeDarkMode(!darkMode)} />
+          <BsFillMoonFill
+            onClick={() => {
+              setPath(path);
+              changeDarkMode(!darkMode);
+            }}
+          />
         )}
       </button>
     </nav>
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,22 +2,40 @@ import React, { useState } from "react";
 
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
-import Link from "next/link";
+// import Link from "next/link";
+
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Sidebar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const { isDark, path } = props;
+  const { isDark } = props;
+  const [path, setPath] = useState("b_home");
 
   const changeSidebarStatus = () => {
     setIsOpen(!isOpen);
   };
+
+  const links = [
+    { id: 1, href: "home", path: "b_home", tx: "Home" },
+    { id: 2, href: "about", path: "b_about", tx: "About" },
+    { id: 3, href: "projects", path: "b_projects", tx: "Projects" },
+    { id: 4, href: "achievements", path: "b_achievements", tx: "Achievements" },
+  ];
+
+  const navStyle =
+    "z-10 flex items-center max-w-4xl mx-auto -top-1 sticky bg-white/80 dark:bg-gray-900/80";
+  const navStyleBlur =
+    "z-10 flex items-center max-w-4xl mx-auto -top-1 sticky backdrop-blur-sm bg-white/80 dark:bg-gray-900/80";
 
   return (
     <>
       <div className="navbar">
         <FaBars
           className="menu-bars dark:text-white hover:cursor-pointer"
-          onClick={changeSidebarStatus}
+          onClick={() => {
+            document.getElementById("nav").className = navStyle;
+            changeSidebarStatus();
+          }}
         />
       </div>
       <div>
@@ -35,74 +53,46 @@ const Sidebar = (props) => {
           <div className="text-xl -pl-3 py-1 my-5 ">
             <AiOutlineClose
               className="menu-bars dark:text-white hover:cursor-pointer"
-              onClick={changeSidebarStatus}
+              onClick={() => {
+                document.getElementById("nav").className = navStyleBlur;
+                changeSidebarStatus();
+              }}
             />
           </div>
           <ul className="nav-menu-items">
-            <li>
-              <Link
-                href="/"
-                className={
-                  path === "home"
-                    ? isDark
+            {links.map((link) => (
+              <li key={link.id}>
+                <Link
+                  id={link.path}
+                  activeClass="active"
+                  to={link.href}
+                  offset={-90}
+                  onClick={() => {
+                    changeSidebarStatus();
+                    setPath(link.path);
+                    for (let i = 0; i < links.length; i++) {
+                      document.getElementById(links[i].path).className = isDark
+                        ? "nav-text dark"
+                        : "nav-text";
+                    }
+                    document.getElementById(link.path).className = isDark
                       ? "nav-text dark-active"
-                      : "nav-text active"
-                    : isDark
-                    ? "nav-text dark"
-                    : "nav-text"
-                }
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className={
-                  path === "about"
-                    ? isDark
-                      ? "nav-text dark-active"
-                      : "nav-text active"
-                    : isDark
-                    ? "nav-text dark"
-                    : "nav-text"
-                }
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/projects"
-                className={
-                  path === "projects"
-                    ? isDark
-                      ? "nav-text dark-active"
-                      : "nav-text active"
-                    : isDark
-                    ? "nav-text dark"
-                    : "nav-text"
-                }
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/achievements"
-                className={
-                  path === "achievements"
-                    ? isDark
-                      ? "nav-text dark-active"
-                      : "nav-text active"
-                    : isDark
-                    ? "nav-text dark"
-                    : "nav-text"
-                }
-              >
-                Achievements
-              </Link>
-            </li>
+                      : "nav-text active";
+                  }}
+                  className={
+                    path === link.path
+                      ? isDark
+                        ? "nav-text dark-active"
+                        : "nav-text active"
+                      : isDark
+                      ? "nav-text dark"
+                      : "nav-text"
+                  }
+                >
+                  {link.tx}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
         <div className={isOpen ? "overlay active" : "overlay"}></div>

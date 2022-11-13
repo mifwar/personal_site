4c721275fb9b55a54e118e6ef4b811f369b4a979
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2022-11-13 23:09:49 +0700

    initial commit of projects section

---
+++ pages/Projects.js
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <section className="min-h-screen" id="projects">
+      <div className="flex justify-center items-center h-screen -my-16 dark:text-white">
+        Projects Page
+      </div>
+    </section>
+  );
+};
+
+export default Projects;

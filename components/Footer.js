138d436976e88b824a48b2a3b714ffa7df53ac1a
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 16:22:22 +0700

    fix bug at customLink

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,17 @@
-import customLink from "../utils/utils";
+const customLink = (href, text) => (
+  <a
+    className="
+          text-blue-600
+          hover:text-blue-400
+          dark:text-blue-400
+          dark:hover:text-blue-300"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {text}
+  </a>
+);
 
 const Footer = () => {
   return (
--- pages/about/index.js
+++ pages/about/index.js
@@ -7,7 +7,21 @@ import { useThemeContext } from "../../utils/darkMode";
 import Navbar from "../../components/Navbar";
 import Social from "../../components/Social";
 import Footer from "../../components/Footer";
-import customLink from "../../utils/utils";
+
+const customLink = (href, text) => (
+  <a
+    className="
+          text-blue-600
+          hover:text-blue-400
+          dark:text-blue-400
+          dark:hover:text-blue-300"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {text}
+  </a>
+);
 
 const About = () => {
   const [darkMode, setDarkMode] = useThemeContext();

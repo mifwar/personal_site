0b024f0d8f484aed2331207ab7107f650d401492
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 15:58:55 +0700

    add customLink

---
+++ utils/utils.js
@@ -0,0 +1,14 @@
+export const customLink = (href, text) => (
+  <a
+    className="
+          text-blue-600
+          hover:text-blue-400
+          dark:text-blue-400
+          dark:hover:text-blue-300"
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {text}
+  </a>
+);

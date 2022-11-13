f06942b140636dbb30d0ff6303c4e78b202e5209
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2022-11-13 15:59:36 +0700

    move footerLink to utils and rename it to customLink

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,15 @@
-const footerLink = (href, text) => (
-  <a
-    className="
-        text-blue-600
-        hover:text-blue-400
-        dark:text-blue-400
-        dark:hover:text-blue-300"
-    href={href}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    {text}
-  </a>
-);
+import { customLink } from "../utils/utils";
 
 const Footer = () => {
   return (
     <div className="flex justify-center text-center flex-col">
       <h4 className="text-xs text-black dark:text-white font-Poppins mt-7 py-1">
         Hosted in
-        {footerLink("https://vercel.com/", " Vercel ")}
+        {customLink("https://vercel.com/", " Vercel ")}
         and built using
-        {footerLink("https://nextjs.org/", " Next.js ")}
+        {customLink("https://nextjs.org/", " Next.js ")}
         and
-        {footerLink("https://tailwindcss.com/", " Tailwind CSS")}
+        {customLink("https://tailwindcss.com/", " Tailwind CSS")}
       </h4>
       <h4 className="text-xs text-black dark:text-white font-Poppins py-1">
         MIT License © 2022 Miftahul Anwar

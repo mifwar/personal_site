eab11f8e45f21b56069cfe1032588879b8883a7b
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2022-11-13 23:09:40 +0700

    initial commit of achievements section

---
+++ pages/Achievements.js
@@ -0,0 +1,11 @@
+const Achievements = () => {
+  return (
+    <section className="min-h-screen" id="achievements">
+      <div className="flex justify-center items-center h-screen -my-16 dark:text-white">
+        Achievements Page
+      </div>
+    </section>
+  );
+};
+
+export default Achievements;

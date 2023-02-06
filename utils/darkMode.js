fba0f635aa9d56a0d129674c7d89760f30adbc04
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-02-07 04:45:17 +0700

    add localStorage

--- utils/darkMode.js
+++ utils/darkMode.js
@@ -1,9 +1,21 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const Context = createContext();
 
 export const DarkMode = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const state = JSON.parse(localStorage.getItem('darkMode'));
+    if (state) {
+      setDarkMode(state);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
   return (
     <Context.Provider value={[darkMode, setDarkMode]}>
       {children}
